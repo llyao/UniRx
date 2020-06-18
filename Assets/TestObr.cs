1c2093e20710346aea8f37e421806dc1cd309806
yaolinglong <a13524701690@163.com>
2020-06-18 21:34:46 +0800

    1

---
+++ Assets/TestObr.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class TestObr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Observable.Range(1, 5)
+                .Scan(100, (x , y) => x + y)
+                .Subscribe(x =>
+                {
+                    Debug.Log(x);
+                });
+    }
+}
